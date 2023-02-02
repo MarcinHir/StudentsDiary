5b70cdcf88d13de8482caf55ae7b419e5128fb69
MarcinHir <hirek87@gmail.com>
2023-02-02 23:26:30 +0100

    Add project files.

---
+++ StudentsDiary/AddEditStudents.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace StudentsDiary
+{
+    partial class AddEditStudents
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbId = new System.Windows.Forms.TextBox();
+            this.lbId = new System.Windows.Forms.Label();
+            this.lbFirstName = new System.Windows.Forms.Label();
+            this.tbFirstName = new System.Windows.Forms.TextBox();
+            this.lbLastName = new System.Windows.Forms.Label();
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.lbMath = new System.Windows.Forms.Label();
+            this.tbMath = new System.Windows.Forms.TextBox();
+            this.lbPhysic = new System.Windows.Forms.Label();
+            this.tbPhysic = new System.Windows.Forms.TextBox();
+            this.lbTechnology = new System.Windows.Forms.Label();
+            this.tbTechnology = new System.Windows.Forms.TextBox();
+            this.lbPolishLang = new System.Windows.Forms.Label();
+            this.tbPolishLang = new System.Windows.Forms.TextBox();
+            this.lbForeignLang = new System.Windows.Forms.Label();
+            this.tbforeignLang = new System.Windows.Forms.TextBox();
+            this.rtbComments = new System.Windows.Forms.RichTextBox();
+            this.lbComments = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbId
+            // 
+            this.tbId.Location = new System.Drawing.Point(126, 12);
+            this.tbId.Name = "tbId";
+            this.tbId.ReadOnly = true;
+            this.tbId.Size = new System.Drawing.Size(199, 22);
+            this.tbId.TabIndex = 0;
+            // 
+            // lbId
+            // 
+            this.lbId.AutoSize = true;
+            this.lbId.Location = new System.Drawing.Point(38, 18);
+            this.lbId.Name = "lbId";
+            this.lbId.Size = new System.Drawing.Size(18, 16);
+            this.lbId.TabIndex = 1;
+            this.lbId.Text = "Id";
+            // 
+            // lbFirstName
+            // 
+            this.lbFirstName.AutoSize = true;
+            this.lbFirstName.Location = new System.Drawing.Point(38, 46);
+            this.lbFirstName.Name = "lbFirstName";
+            this.lbFirstName.Size = new System.Drawing.Size(38, 16);
+            this.lbFirstName.TabIndex = 3;
+            this.lbFirstName.Text = "Imię :";
+            // 
+            // tbFirstName
+            // 
+            this.tbFirstName.Location = new System.Drawing.Point(126, 40);
+            this.tbFirstName.Name = "tbFirstName";
+            this.tbFirstName.Size = new System.Drawing.Size(199, 22);
+            this.tbFirstName.TabIndex = 2;
+            // 
+            // lbLastName
+            // 
+            this.lbLastName.AutoSize = true;
+            this.lbLastName.Location = new System.Drawing.Point(38, 74);
+            this.lbLastName.Name = "lbLastName";
+            this.lbLastName.Size = new System.Drawing.Size(71, 16);
+            this.lbLastName.TabIndex = 5;
+            this.lbLastName.Text = "Nazwisko :";
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.Location = new System.Drawing.Point(126, 68);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(199, 22);
+            this.tbLastName.TabIndex = 4;
+            // 
+            // lbMath
+            // 
+            this.lbMath.AutoSize = true;
+            this.lbMath.Location = new System.Drawing.Point(38, 102);
+            this.lbMath.Name = "lbMath";
+            this.lbMath.Size = new System.Drawing.Size(87, 16);
+            this.lbMath.TabIndex = 7;
+            this.lbMath.Text = "Matematyka :";
+            // 
+            // tbMath
+            // 
+            this.tbMath.Location = new System.Drawing.Point(126, 96);
+            this.tbMath.Name = "tbMath";
+            this.tbMath.Size = new System.Drawing.Size(199, 22);
+            this.tbMath.TabIndex = 6;
+            // 
+            // lbPhysic
+            // 
+            this.lbPhysic.AutoSize = true;
+            this.lbPhysic.Location = new System.Drawing.Point(38, 130);
+            this.lbPhysic.Name = "lbPhysic";
+            this.lbPhysic.Size = new System.Drawing.Size(52, 16);
+            this.lbPhysic.TabIndex = 9;
+            this.lbPhysic.Text = "Fizyka :";
+            // 
+            // tbPhysic
+            // 
+            this.tbPhysic.Location = new System.Drawing.Point(126, 124);
+            this.tbPhysic.Name = "tbPhysic";
+            this.tbPhysic.Size = new System.Drawing.Size(199, 22);
+            this.tbPhysic.TabIndex = 8;
+            // 
+            // lbTechnology
+            // 
+            this.lbTechnology.AutoSize = true;
+            this.lbTechnology.Location = new System.Drawing.Point(38, 158);
+            this.lbTechnology.Name = "lbTechnology";
+            this.lbTechnology.Size = new System.Drawing.Size(83, 16);
+            this.lbTechnology.TabIndex = 11;
+            this.lbTechnology.Text = "Technologia";
+            // 
+            // tbTechnology
+            // 
+            this.tbTechnology.Location = new System.Drawing.Point(126, 152);
+            this.tbTechnology.Name = "tbTechnology";
+            this.tbTechnology.Size = new System.Drawing.Size(199, 22);
+            this.tbTechnology.TabIndex = 10;
+            // 
+            // lbPolishLang
+            // 
+            this.lbPolishLang.AutoSize = true;
+            this.lbPolishLang.Location = new System.Drawing.Point(38, 186);
+            this.lbPolishLang.Name = "lbPolishLang";
+            this.lbPolishLang.Size = new System.Drawing.Size(81, 16);
+            this.lbPolishLang.TabIndex = 13;
+            this.lbPolishLang.Text = "Język polski";
+            // 
+            // tbPolishLang
+            // 
+            this.tbPolishLang.Location = new System.Drawing.Point(126, 180);
+            this.tbPolishLang.Name = "tbPolishLang";
+            this.tbPolishLang.Size = new System.Drawing.Size(199, 22);
+            this.tbPolishLang.TabIndex = 12;
+            // 
+            // lbForeignLang
+            // 
+            this.lbForeignLang.AutoSize = true;
+            this.lbForeignLang.Location = new System.Drawing.Point(38, 214);
+            this.lbForeignLang.Name = "lbForeignLang";
+            this.lbForeignLang.Size = new System.Drawing.Size(75, 16);
+            this.lbForeignLang.TabIndex = 15;
+            this.lbForeignLang.Text = "Język obcy";
+            // 
+            // tbforeignLang
+            // 
+            this.tbforeignLang.Location = new System.Drawing.Point(126, 208);
+            this.tbforeignLang.Name = "tbforeignLang";
+            this.tbforeignLang.Size = new System.Drawing.Size(199, 22);
+            this.tbforeignLang.TabIndex = 14;
+            // 
+            // rtbComments
+            // 
+            this.rtbComments.Location = new System.Drawing.Point(126, 249);
+            this.rtbComments.Name = "rtbComments";
+            this.rtbComments.Size = new System.Drawing.Size(199, 116);
+            this.rtbComments.TabIndex = 16;
+            this.rtbComments.Text = "";
+            // 
+            // lbComments
+            // 
+            this.lbComments.AutoSize = true;
+            this.lbComments.Location = new System.Drawing.Point(38, 252);
+            this.lbComments.Name = "lbComments";
+            this.lbComments.Size = new System.Drawing.Size(45, 16);
+            this.lbComments.TabIndex = 17;
+            this.lbComments.Text = "Uwagi";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(126, 393);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 18;
+            this.btnCancel.Text = "Anuluj";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(250, 393);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(75, 23);
+            this.btnConfirm.TabIndex = 19;
+            this.btnConfirm.Text = "Zatwierdź";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // AddEditStudents
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 459);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.lbComments);
+            this.Controls.Add(this.rtbComments);
+            this.Controls.Add(this.lbForeignLang);
+            this.Controls.Add(this.tbforeignLang);
+            this.Controls.Add(this.lbPolishLang);
+            this.Controls.Add(this.tbPolishLang);
+            this.Controls.Add(this.lbTechnology);
+            this.Controls.Add(this.tbTechnology);
+            this.Controls.Add(this.lbPhysic);
+            this.Controls.Add(this.tbPhysic);
+            this.Controls.Add(this.lbMath);
+            this.Controls.Add(this.tbMath);
+            this.Controls.Add(this.lbLastName);
+            this.Controls.Add(this.tbLastName);
+            this.Controls.Add(this.lbFirstName);
+            this.Controls.Add(this.tbFirstName);
+            this.Controls.Add(this.lbId);
+            this.Controls.Add(this.tbId);
+            this.Name = "AddEditStudents";
+            this.Text = "AddEditStudents";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbId;
+        private System.Windows.Forms.Label lbId;
+        private System.Windows.Forms.Label lbFirstName;
+        private System.Windows.Forms.TextBox tbFirstName;
+        private System.Windows.Forms.Label lbLastName;
+        private System.Windows.Forms.TextBox tbLastName;
+        private System.Windows.Forms.Label lbMath;
+        private System.Windows.Forms.TextBox tbMath;
+        private System.Windows.Forms.Label lbPhysic;
+        private System.Windows.Forms.TextBox tbPhysic;
+        private System.Windows.Forms.Label lbTechnology;
+        private System.Windows.Forms.TextBox tbTechnology;
+        private System.Windows.Forms.Label lbPolishLang;
+        private System.Windows.Forms.TextBox tbPolishLang;
+        private System.Windows.Forms.Label lbForeignLang;
+        private System.Windows.Forms.TextBox tbforeignLang;
+        private System.Windows.Forms.RichTextBox rtbComments;
+        private System.Windows.Forms.Label lbComments;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnConfirm;
+    }
+}
